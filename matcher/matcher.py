c0a0991f4c9f382a4564fc145fdaedb00528821f
NeluinWriter <108116000+NeluinWriter@users.noreply.github.com>
2025-06-04 09:27:14 +0900

    Commit

--- matcher/matcher.py
+++ matcher/matcher.py
@@ -1,13 +1,8 @@
-from matcher.utils.scoring_utils import ScoringService
-from matcher.services.matching_service import MatchingService
 from matcher.ui.matcher_window import MatcherWindow
 
 
 def start(root):
-    scoring_service = ScoringService()
-    matching_service = MatchingService(scoring_service)
-
     root.title("룸메이트 매칭 시스템")
 
-    matcher_window = MatcherWindow(root, matching_service)
+    matcher_window = MatcherWindow(root)
     matcher_window.show_password_verification()
